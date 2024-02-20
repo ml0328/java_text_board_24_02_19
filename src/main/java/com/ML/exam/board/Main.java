8a1a1a8a2cdbe97997aae9603b9621dd36de30d9
ml0328 <minjeelyu111100@gmail.com>
2024-02-20 22:19:20 +0900

    명령어 입력받기

--- src/main/java/com/ML/exam/board/Main.java
+++ src/main/java/com/ML/exam/board/Main.java
@@ -1,7 +1,18 @@
 package com.ML.exam.board;
 
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println( "== 자바 텍스트 게시판 0.1v ==");
+    System.out.println("== 자바 텍스트 게시판 시작 ==");
+
+    Scanner sc = new Scanner(System.in);
+    System.out.printf("명령 : ");
+    String cmd = sc.nextLine();
+    System.out.printf("입력받은 명령어 : %s\n", cmd);
+    System.out.println( "== 자바 텍스트 게시판 종료 ==");
+
+    sc.close();
   }
 }
