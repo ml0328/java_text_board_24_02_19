2b59266b7af1a64cc6b0e86c4f51edcab6368999
ml0328 <minjeelyu111100@gmail.com>
2024-02-20 23:35:12 +0900

    exit를 입력 받기 전까지 계속 입력

--- src/main/java/com/ML/exam/board/Main.java
+++ src/main/java/com/ML/exam/board/Main.java
@@ -7,12 +7,21 @@ public class Main {
     System.out.println( "== 자바 텍스트 게시판 0.1v ==");
     System.out.println("== 자바 텍스트 게시판 시작 ==");
 
-    Scanner sc = new Scanner(System.in);
-    System.out.printf("명령 : ");
-    String cmd = sc.nextLine();
-    System.out.printf("입력받은 명령어 : %s\n", cmd);
-    System.out.println( "== 자바 텍스트 게시판 종료 ==");
+    Scanner sc = new Scanner (System.in);
 
+    while (true) {
+      System.out.printf("명령 )");
+      String cmd = sc.nextLine();
+
+      if (cmd.equals("exit")) {
+        System.out.println("프로그램을 종료합니다.");
+        break;
+      }
+
+      System.out.printf("입력받은 명령어 : %s\n", cmd);
+    }
+    System.out.println("== 자바 텍스트 게시판 종료==");
     sc.close();
+
   }
 }
