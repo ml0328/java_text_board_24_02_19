0ec1351416b67028fb16ed413b71c491cdcc672a
ml0328 <minjeelyu111100@gmail.com>
2024-02-21 11:28:23 +0900

    생성자를 이용해서 게시물 객체를 쉽게 만들기

--- src/main/java/com/ML/exam/board/Main.java
+++ src/main/java/com/ML/exam/board/Main.java
@@ -23,10 +23,8 @@ public class Main {
 
         int id = articleLastId + 1;
 
-        Article article = new Article();
-        article.id = id;
-        article.title = title;
-        article.body= body;
+        Article article = new Article(id, title, body);
+
 
         System.out.println("생성 된 게시물 객체 : " + article);
         System.out.printf("%d번 게시물이 생성되었습니다.\n", article.id);
@@ -50,6 +48,11 @@ class Article {
   int id;
   String title;
   String body;
+  Article (int id, String title, String body) {
+    this.id = id;
+    this.title = title;
+    this.body = body;
+  }
   public String toString () {
     return String.format ("{id : %d, title : \"%s\", body : \"%s\"}", id, title, body);
   }
